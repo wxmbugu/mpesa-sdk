44aba1a649116711411a92accb6a886ea2a75424
wambug <wambugumacharia35@gmail.com>
2022-07-21 04:29:28 +0300

    access_token 1/2

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 use hyper::{body::HttpBody, Client};
 use hyper_tls::HttpsConnector;
-use std::io::{stdout, Write};
-
+use std::string::ToString;
+use std::{
+    io::{stdout, Write},
+    str::FromStr,
+    //string,
+};
+use strum_macros::Display;
 ///! Mpesa to make mpesa transcations
 #[derive(Debug)]
 pub struct Mpesa {
@@ -10,40 +15,51 @@ pub struct Mpesa {
     production_env: Environment,
 }
 ///!  Production Environment
-#[derive(Debug)]
+#[derive(Debug, Display)]
 pub enum Environment {
     ///!Sandbox app Environment
+    #[strum(
+        serialize = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+    )]
     Sandbox,
     ///! Live app Environment
+    #[strum(
+        serialize = "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+    )]
     Production,
 }
 
-//const SANDBOXBASEURL: &str = "https://sandbox.safaricom.co.ke";
+// const SANDBOXBASEURL: &str =
+//     "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
 
-//const PRODUCTIONBASEURL: &str = "https://api.safaricom.co.ke";
+// const PRODUCTIONBASEURL: &str =
+//     "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+// impl FromStr for Environment {
+//     type Err = ();
 
+//     fn from_str(s: &str) -> Result<Environment, ()> {
+//         match s {
+//             SANDBOXBASEURL => Ok(Environment::Sandbox),
+//             PRODUCTIONBASEURL => Ok(Environment::Production),
+//             _ => Err(()),
+//         }
+//     }
+// }
 impl Mpesa {
     ///! Creates an Mpesa app either sandbox or live app
     pub fn new(key: String, secret: String, env: Environment) -> Mpesa {
-        // match env {
-        //   Environment::Sandbox => SANDBOXBASEURL,
-        // Environment::Production => PRODUCTIONBASEURL,
-        // };
         Mpesa {
             consumerkey: key,
             consumersecret: secret,
             production_env: env,
         }
     }
-    ///! Returns a token to be used to aythenticate an app
-    ///!  
+    ///! Returns a token to be used to authenticate a safaricomapp
+    ///!Sandbox app or Production app
     pub async fn get_access_token(&self) -> Result<(), Box<dyn std::error::Error>> {
         let https = HttpsConnector::new();
         let client = Client::builder().build::<_, hyper::Body>(https);
-
-        let uri = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
-            .parse()?;
-
+        let uri = self.production_env.to_string().parse()?;
         let mut resp = client.get(uri).await?;
         println!("Response: {}", resp.status());
 
